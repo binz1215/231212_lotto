af19662fb6911f6dbd5c99d567a9dec55b0e9ca4
binz1215 <binz1215@naver.com>
2023-12-12 15:10:16 +0900

    입력메서드 작성

---
+++ src/view/inputView.js
@@ -0,0 +1,28 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class InputView {
+  async purchaseAmount() {
+    const purchase = await Console.readLineAsync('구입금액을 입력해 주세요.\n');
+
+    return purchase;
+  }
+
+  async manualCount() {
+    const manual = await Console.readLineAsync('수동 개수를 입력해 주세요.\n');
+
+    return manual;
+  }
+
+  async manualLotto() {
+    const lotto = await Console.readLineAsync('로또 번호를 입력해 주세요.(,로 구분)\n');
+    const arrayLotto = lotto.split(',');
+
+    return arrayLotto;
+  }
+}
+
+export default InputView;
+
+// const input = new InputView();
+// const lotto = await input.manualLotto();
+// console.log(lotto);
