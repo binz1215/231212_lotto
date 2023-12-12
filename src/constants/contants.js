817b06d7e48e998f7c65d9f82c5280abaab6dbac
binz1215 <binz1215@naver.com>
2023-12-12 14:57:00 +0900

    constants 작성

---
+++ src/constants/contants.js
@@ -0,0 +1,24 @@
+const CountNum = {
+  ONE: 1,
+  SIX: 6,
+};
+
+const LottoArange = {
+  MIM: 1,
+  MAX: 45,
+  BONUS: 20,
+};
+
+const MAXAMOUNT = 100000;
+
+const UNIT = 1000;
+
+const PRIZE = {
+  first: 2000000000,
+  second: 30000000,
+  third: 1500000,
+  fourth: 50000,
+  fifth: 5000,
+};
+
+export { CountNum, LottoArange, MAXAMOUNT, UNIT, PRIZE };
